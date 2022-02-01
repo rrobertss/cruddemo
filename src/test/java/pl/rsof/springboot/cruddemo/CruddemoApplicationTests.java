68a1923711ac46dcd8f250e08b635ac01bcb959c
Robert Siwak <rsiwak@gmail.com>
2022-02-01 22:12:52 +0100

    cruddemo: initial

---
+++ src/test/java/pl/rsof/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rsof.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
